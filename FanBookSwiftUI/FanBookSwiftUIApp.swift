feae80928e2fca61fe627a1425e01e23b15bebc4
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-18 09:56:15 +0300

    Initial Commit

---
+++ FanBookSwiftUI/FanBookSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FanBookSwiftUIApp.swift
+//  FanBookSwiftUI
+//
+//  Created by Ömer Faruk Kılıçaslan on 18.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct FanBookSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
