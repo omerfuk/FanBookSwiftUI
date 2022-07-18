da28d70474c1a9a54ba3242016cfda5666eeb179
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-18 10:06:18 +0300

    .

--- FanBookSwiftUI/FavoriteModel.swift
+++ FanBookSwiftUI/FavoriteModel.swift
@@ -22,7 +22,7 @@ struct FavoriteElements : Identifiable {
     var imageName: String
     var description: String
 }
-
+//BANDS
 let metallica = FavoriteElements(name: "Metallica", imageName: "metallica", description: "No 1 Music Group")
 
 let megaDeth = FavoriteElements(name: "Megadeth", imageName: "megadeth", description: "No 2 Music Group")
@@ -30,3 +30,12 @@ let megaDeth = FavoriteElements(name: "Megadeth", imageName: "megadeth", descrip
 let ironMaiden = FavoriteElements(name: "Iron Maiden", imageName: "ironMaiden", description: "No 3 Music Group")
 
 let favoriteBands = FavoriteModel(title: "Favorite Bands", elements: [metallica,megaDeth,ironMaiden])
+
+//MOVIES
+let pulpFiction = FavoriteElements(name: "Pulp Fiction", imageName: "pulpFiction", description: "No 1 Movie")
+
+let theDarkKnight = FavoriteElements(name: "The Dark Knight", imageName: "darkKnight", description: "No 2 Movie")
+
+let killBill = FavoriteElements(name: "Kill Bill", imageName: "killBill", description: "No 3 Movie")
+
+let favoriteMovies = FavoriteModel(title: "Favorite Movies", elements: [pulpFiction,theDarkKnight,killBill])
